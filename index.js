e49bc2d8d8f736c25a9d032199fc40fb4af3d4b1
Gabriel <gaby_lamuerte@hotmail.com>
2023-09-13 11:22:26 -0300

    añadí un js que abré whatsapp al apretar Reserva

---
+++ index.js
@@ -0,0 +1,7 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const reservaBtn = document.getElementById("reservaBtn");
+
+    reservaBtn.addEventListener("click",() => {
+        window.open("https://wa.me/3517540783");
+    });
+});
